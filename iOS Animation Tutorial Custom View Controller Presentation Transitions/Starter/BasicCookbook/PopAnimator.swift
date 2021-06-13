a244ef5ddb966878a668ee31ac5d51ccc6cfc3e4
woookdev <hjw0911@gmail.com>
2021-06-13 20:15:58 +0900

    Add transition animation and completeTransition

--- iOS Animation Tutorial Custom View Controller Presentation Transitions/Starter/BasicCookbook/PopAnimator.swift
+++ iOS Animation Tutorial Custom View Controller Presentation Transitions/Starter/BasicCookbook/PopAnimator.swift
@@ -32,11 +32,28 @@ class PopAnimator:
   NSObject,
   UIViewControllerAnimatedTransitioning
 {
+  let duration = 0.8
+  var presenting = true
+  var originFrame = CGRect.zero
+  
+  
+  
   func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-    return 0
+    return duration
   }
   
   func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+    let containerView = transitionContext.containerView
+    let toView = transitionContext.view(forKey: .to)!
+    let recipeView = presenting ? toView : transitionContext.view(forKey: .from)
     
+    containerView.addSubview(toView)
+    toView.transform = CGAffineTransform(scaleX: 0.0, y: 0.0)
+    UIView.animate(
+      withDuration: duration) {
+      toView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+    } completion: { _ in
+      transitionContext.completeTransition(true)
+    }
   }
 }
